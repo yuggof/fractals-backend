3dd5f9290470dc6c0c89b8ea929af94262b02b18
yuggof <yuggof0@gmail.com>
2016-08-28 18:34:49 +0000

    dirty generate fractal job implementation

--- app/jobs/generate_fractal_job.rb
+++ app/jobs/generate_fractal_job.rb
@@ -1,6 +1,12 @@
 class GenerateFractalJob < ApplicationJob
   def perform(fractal_id)
     f = Fractal.find(fractal_id)
+
+    gfbp = Rails.root.join('bin', 'generate-fractal')
+    gfop = Rails.root.join('fractals', "#{f.id}.png")
+
+    `#{gfbp} -o #{gfop}`
+
     f.update!(finished_at: Time.zone.now)
   end
 end
